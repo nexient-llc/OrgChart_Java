de1eb75b8a966b564e4a14423df0d6adaa716294
Jessica Harper <jharper@systemsinmotion.com>
2013-02-20 14:52:21 -0500

    Code clean up - removing unused imports, removing unused tiles and view listings, removing commented out sections, etc

--- src/test/java/com/systemsinmotion/orgchart/service/DepartmentServiceTest.java
+++ src/test/java/com/systemsinmotion/orgchart/service/DepartmentServiceTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
