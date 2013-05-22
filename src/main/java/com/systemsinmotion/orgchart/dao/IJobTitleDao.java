aee404aec1f68872b9d32f6b8b07e5a4b895b441
Steven Byks <sbyks@systemsinmotion.com>
2013-05-22 11:52:49 -0400

    Added JobTitle DAO and interface

---
+++ src/main/java/com/systemsinmotion/orgchart/dao/IJobTitleDao.java
@@ -0,0 +1,64 @@
+package com.systemsinmotion.orgchart.dao;
+
+import java.util.List;
+
+import com.systemsinmotion.orgchart.entity.JobTitle;
+
+public interface IJobTitleDao {
+
+	/**
+	 * Deletes the <code>JobTitle</code> instance from the database.
+	 * 
+	 * @param jobTitle
+	 *            JobTitle instance to delete
+	 */
+	public abstract void delete(JobTitle jobTitle);
+
+	/**
+	 * Returns all <code>JobTitle</code> instances as a <code>List</code>
+	 * 
+	 * @return List of JobTitles
+	 */
+	public abstract List<JobTitle> findAll();
+
+	/**
+	 * Returns a <code>JobTitle</code> instance having a given <code>id</code>
+	 * value.
+	 * 
+	 * @param id
+	 *            Identifier of <code>JobTitle</code> instance to find
+	 * @return Single <code>JobTitle</code> instance. Null if not found.
+	 */
+	public abstract JobTitle findById(Integer id);
+
+	/**
+	 * Returns a <code>JobTitle</code> instance with a given <code>name</code>
+	 * value.
+	 * 
+	 * @param name
+	 *            Name of the <code>JobTitle</code> to find
+	 * @return Instance of <code>JobTitle</code>. Null if not found.
+	 */
+	public abstract JobTitle findByName(String name);
+
+	/**
+	 * Saves a give <code>JobTitle</code instance and returns its generated
+	 * id.
+	 * 
+	 * @param jobTitle
+	 *            The <code>JobTitle</code> instance to be saved
+	 * @return The identifier (id) of the new <code>JobTitle</code> instance.
+	 */
+	public abstract Integer save(JobTitle jobTitle);
+
+	/**
+	 * Updates an existing <code>JobTitle</code> instance with new values.
+	 * 
+	 * @param jobTitle
+	 *            The <code>JobTitle</code> instance to be updated
+	 */
+	public abstract void update(JobTitle jobTitle);
+
+	public abstract String toString();
+
+}
---
+++ src/main/java/com/systemsinmotion/orgchart/dao/JobTitleDao.java
@@ -0,0 +1,132 @@
+package com.systemsinmotion.orgchart.dao;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import com.systemsinmotion.orgchart.entity.Department;
+import com.systemsinmotion.orgchart.entity.JobTitle;
+
+/**
+ * A data access object (DAO) providing persistence and search support for JobTitle entities. Transaction control of the
+ * save(), update() and delete() operations must be handled externally by senders of these methods or must be manually
+ * added to each of these methods for data to be persisted to the JPA datastore.
+ * 
+ * @see com.systemsinmotion.orgchart.entity.JobTitle
+ * @author Steven Byks
+ */
+@Repository("jobTitleDao")
+public class JobTitleDao implements IJobTitleDao {
+
+	/**
+	 * @see org.slf4j.Logger
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(JobTitleDao.class);
+
+	/**
+	 * @see org.springframework.orm.hibernate3.HibernateTemplate
+	 */
+	@Autowired
+	HibernateTemplate hibernateTemplate;
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.JobTitleDao#delete(com.systemsinmotion.orgchart.entity.JobTitle)
+	 */
+	@Override
+	public void delete(JobTitle jobTitle) {
+		LOG.debug("deleting JobTitle instance with name: " + jobTitle.getName());
+		this.hibernateTemplate.delete(jobTitle);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.JobTitleDao#findAll()
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<JobTitle> findAll() {
+		LOG.debug("finding all JobTitle instances");
+		return this.hibernateTemplate.find("from " + JobTitle.class.getName() + " order by name");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.DepartmentDao#findById(java.lang.Integer)
+	 */
+	@Override
+	public JobTitle findById(Integer id) {
+		LOG.debug("getting JobTitle instance with id: " + id);
+		JobTitle jobTitle = null;
+
+		if (id != null) {
+			jobTitle = this.hibernateTemplate.get(JobTitle.class, id);
+		}
+		return jobTitle;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.JobTitleDao#findByName(java.lang.String)
+	 */
+	@Override
+	public JobTitle findByName(String name) {
+		LOG.debug("finding JobTitle instance by name: " + name);
+		JobTitle jobTitle = null;
+
+		if (StringUtils.hasText(name)) {
+			DetachedCriteria criteria = DetachedCriteria.forClass(JobTitle.class);
+			criteria.add(Restrictions.eq("name", name));
+			criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+
+			@SuppressWarnings("unchecked")
+			List<JobTitle> jobTitles = this.hibernateTemplate.findByCriteria(criteria);
+
+			if (jobTitles != null && !jobTitles.isEmpty()) {
+				jobTitle = jobTitles.get(0);
+			}
+		}
+		return jobTitle;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.JobTitleDao#save(com.systemsinmotion.orgchart.entity.JobTitle)
+	 */
+	@Override
+	public Integer save(JobTitle jobTitle) {
+		LOG.debug("saving JobTitle instance with name: " + jobTitle.getName());
+		return (Integer) this.hibernateTemplate.save(jobTitle);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.JobTitleDao#update(com.systemsinmotion.orgchart.entity.JobTitle)
+	 */
+	@Override
+	public void update(JobTitle jobTitle) {
+		LOG.debug("updating JobTitle instance with name: " + jobTitle.getName());
+		this.hibernateTemplate.update(jobTitle);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.JobTitleDao#toString()
+	 */
+	@Override
+	public String toString() {
+		String newLine = System.getProperty("line.separator");
+		return getClass().getName() + " {" + newLine + "\thibernateTemplate : " + this.hibernateTemplate.toString()
+				+ newLine + "}";
+	}
+
+}
