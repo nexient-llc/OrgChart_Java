f57807824dcf0ce547cbe2955619875eda8a57c5
Steven Byks <sbyks@systemsinmotion.com>
2013-06-04 15:18:19 -0400

    Various project wide changes to support latest code changes.

--- src/main/java/com/systemsinmotion/orgchart/web/View.java
+++ src/main/java/com/systemsinmotion/orgchart/web/View.java
@@ -1,10 +1,10 @@
 package com.systemsinmotion.orgchart.web;
 
 public class View {
-	public static final String HOME        = "home";
+	public static final String HOME = "home";
 	public static final String DEPARTMENTS = "depts";
-	public static final String JOB_TITLES  = "jobs";
-	public static final String EMPLOYEES   = "emps";
+	public static final String JOB_TITLES = "jobs";
+	public static final String EMPLOYEES = "emps";
 	public static final String LOGIN = "login";
 	public static final String ADMIN_DEFAULT = "admin/default";
 	public static final String ADMIN_LOGIN = "admin/login";
