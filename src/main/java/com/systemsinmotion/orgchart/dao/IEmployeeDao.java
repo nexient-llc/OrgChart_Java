36a236c63adfc8b59d9a1106c9cecf0d66b383fa
Vitaly Peker <vpeker@systemsinmotion.com>
2013-06-05 13:54:33 -0400

    Added find by last and first name and job title

--- src/main/java/com/systemsinmotion/orgchart/dao/IEmployeeDao.java
+++ src/main/java/com/systemsinmotion/orgchart/dao/IEmployeeDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.systemsinmotion.orgchart.entity.Department;
 import com.systemsinmotion.orgchart.entity.Employee;
+import com.systemsinmotion.orgchart.entity.JobTitle;
 
 public interface IEmployeeDao {
 
@@ -19,10 +20,16 @@ public interface IEmployeeDao {
 
 	public abstract List<Employee> findByDept(Department department);
 
+	public abstract List<Employee> findByJobTitle(JobTitle jobTitle);
+
 	public abstract List<Employee> findByIsManager(Boolean isManager);
 
 	public abstract List<Employee> findByManagerId(Integer managerId);
 
 	public abstract Employee findByEmail(String email);
+
+	public abstract List<Employee> findByLastName(String lastName);
+
+	public abstract List<Employee> findByFirstName(String firstName);
 
 }
