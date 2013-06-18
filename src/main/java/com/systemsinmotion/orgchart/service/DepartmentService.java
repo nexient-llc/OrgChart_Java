f9edfdd39d65e6d11c562f1062cdc954986fd84f
dmcmillen <dmcmillen@lt-CNU2103GK9.c.systemsinmotion.com>
2013-06-18 12:57:22 -0400

    Update the POM file and dao-context.xml. Built project using Maven with the goals. Corrected Errors in class DepartmentDao.

--- src/main/java/com/systemsinmotion/orgchart/service/DepartmentService.java
+++ src/main/java/com/systemsinmotion/orgchart/service/DepartmentService.java
@@ -5,35 +5,35 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.systemsinmotion.orgchart.dao.DepartmentDAO;
+import com.systemsinmotion.orgchart.dao.DepartmentDao;
 import com.systemsinmotion.orgchart.entity.Department;
 
 @Service("departmentService")
 public class DepartmentService {
 
 	@Autowired
-	DepartmentDAO departmentDAO;
+	DepartmentDao DepartmentDao;
 
 	public Department findDepartmentByID(Integer departmentId) {
 
-		return this.departmentDAO.findById(departmentId);
+		return this.DepartmentDao.findById(departmentId);
 	}
 
-	public void setDepartmentDAO(DepartmentDAO deparmentDAO) {
-		this.departmentDAO = deparmentDAO;
+	public void setDepartmentDAO(DepartmentDao deparmentDAO) {
+		this.DepartmentDao = deparmentDAO;
 	}
 
 	public List<Department> findAllDepartments() {
-		return this.departmentDAO.findAll();
+		return this.DepartmentDao.findAll();
 
 	}
 
 	public Integer storeDepartment(Department department) {
-		return this.departmentDAO.save(department);
+		return this.DepartmentDao.save(department);
 	}
 
 	public void removeDepartment(Department department) {
-		this.departmentDAO.delete(department);
+		this.DepartmentDao.delete(department);
 	}
 
 }
