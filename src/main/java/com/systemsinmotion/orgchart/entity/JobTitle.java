bc28d9b3bd326a238457e6137ff0c90c8f0657fe
Joel Dollarhide <jdollarhide@systemsinmotion.com>
2013-08-19 11:14:38 -0400

    Added in the entity for the JobTitle Table.

---
+++ src/main/java/com/systemsinmotion/orgchart/entity/JobTitle.java
@@ -0,0 +1,39 @@
+package com.systemsinmotion.orgchart.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table (name = "JOB_TITLE")
+public class JobTitle {
+	
+	private int id;
+	private String name;
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	@Column (name = "ID", nullable = false, unique = true)
+	public int getId() {
+		return id;
+	}
+	
+	@Column (name = "NAME", nullable = false, length = 50)
+	public String getName() {
+		return name;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
