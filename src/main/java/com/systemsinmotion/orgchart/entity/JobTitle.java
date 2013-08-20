5509f409353087f2d0c5ab589b838dca3bc7f2fe
Joel Dollarhide <jdollarhide@systemsinmotion.com>
2013-08-20 14:21:56 -0400

    Added jobTitle and Employee to the spring xml

--- src/main/java/com/systemsinmotion/orgchart/entity/JobTitle.java
+++ src/main/java/com/systemsinmotion/orgchart/entity/JobTitle.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,7 +16,6 @@ public class JobTitle {
 	
 	@Id
 	@GeneratedValue (strategy = GenerationType.AUTO)
-	@OneToMany (mappedBy="job_title_id")
 	@Column (name = "ID", nullable = false, unique = true)
 	public Integer getId() {
 		return id;
