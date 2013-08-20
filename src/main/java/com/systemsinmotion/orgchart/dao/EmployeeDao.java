ad6f893de562e340d54aed8575e41e2bbd64ea96
Joel Dollarhide <jdollarhide@systemsinmotion.com>
2013-08-20 11:52:57 -0400

    Added addtional code to the EmployeeDao functions.

--- src/main/java/com/systemsinmotion/orgchart/dao/EmployeeDao.java
+++ src/main/java/com/systemsinmotion/orgchart/dao/EmployeeDao.java
@@ -1,12 +1,18 @@
 package com.systemsinmotion.orgchart.dao;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.systemsinmotion.orgchart.entity.Department;
 import com.systemsinmotion.orgchart.entity.Employee;
@@ -34,7 +40,6 @@ public class EmployeeDao implements IEmployeeDao {
 
 	@Override
 	public Employee findById(Integer id) {
-		// TODO Auto-generated method stub
 		LOG.debug("getting Employee instance with id: " + id);
 		Employee empl = null;
 		
@@ -45,21 +50,56 @@ public class EmployeeDao implements IEmployeeDao {
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public List<Employee> findByFirstName(String first_name) {
-		// TODO Auto-generated method stub
-		return null;
+		LOG.debug("finding Employee instances by first name: " + first_name);
+		List<Employee> empl = Collections.EMPTY_LIST;
+		
+		if(StringUtils.hasText(first_name)){
+			DetachedCriteria criteria = DetachedCriteria.forClass(Employee.class);
+			criteria.add(Restrictions.eq("first_name", first_name));
+			criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+			
+			List<Employee>employees = this.hibernateTemplate.findByCriteria(criteria);
+			empl.addAll(employees);
+		}
+		return empl;
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public List<Employee> findByLastName(String last_name) {
-		// TODO Auto-generated method stub
-		return null;
+		LOG.debug("finding Employee instances by last name: " + last_name);
+		List<Employee> empl = Collections.EMPTY_LIST;
+		
+		if(StringUtils.hasText(last_name)){
+			DetachedCriteria criteria = DetachedCriteria.forClass(Employee.class);
+			criteria.add(Restrictions.eq("first_name", last_name));
+			criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+			
+			List<Employee>employees = this.hibernateTemplate.findByCriteria(criteria);
+			empl.addAll(employees);
+		}
+		return empl;
 	}
 
 	@Override
 	public Employee findByEmail(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		Employee empl = null;
+		
+		if (StringUtils.hasText(email)){
+			DetachedCriteria criteria = DetachedCriteria.forClass(Employee.class);
+			criteria.add(Restrictions.eq("email", email));
+			criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
+			
+			@SuppressWarnings("unchecked")
+			List<Employee> employees = this.hibernateTemplate.findByCriteria(criteria);
+			
+			if(null != employees && !employees.isEmpty()){
+				empl = employees.get(0);
+			}
+		}
+		return empl;
 	}
 
 	@Override
