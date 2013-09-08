ff4e012631c80e4362c80e23498914a364456237
Tony Wamsley <awamsley@systemsinmotion.com>
2013-09-07 23:35:02 -0400

    Activated DepartmentServiceTest

--- src/main/java/com/systemsinmotion/orgchart/service/DepartmentService.java
+++ src/main/java/com/systemsinmotion/orgchart/service/DepartmentService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.systemsinmotion.orgchart.dao.DepartmentDao;
 import com.systemsinmotion.orgchart.dao.IDepartmentDao;
 import com.systemsinmotion.orgchart.entity.Department;
 
