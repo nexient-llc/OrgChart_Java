e4ae5d39fc26114a7bcd188c90f3729ac500cae3
skronek <kskronek@systemsinmotion.com>
2013-11-03 00:42:21 -0400

    reference class DepartmentDAO renamed to DepartmentDao

--- src/main/java/com/systemsinmotion/orgchart/service/DepartmentService.java
+++ src/main/java/com/systemsinmotion/orgchart/service/DepartmentService.java
@@ -5,21 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.systemsinmotion.orgchart.dao.DepartmentDAO;
+import com.systemsinmotion.orgchart.dao.IDepartmentDao;
 import com.systemsinmotion.orgchart.entity.Department;
 
 @Service("departmentService")
 public class DepartmentService {
 
 	@Autowired
-	DepartmentDAO departmentDAO;
+	IDepartmentDao departmentDAO;
 
 	public Department findDepartmentByID(Integer departmentId) {
 
 		return this.departmentDAO.findById(departmentId);
 	}
 
-	public void setDepartmentDAO(DepartmentDAO deparmentDAO) {
+	public void setDepartmentDAO(IDepartmentDao deparmentDAO) {
 		this.departmentDAO = deparmentDAO;
 	}
 
