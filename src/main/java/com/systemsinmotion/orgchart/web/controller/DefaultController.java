be2ef5cfb6285b6acb1a4cee60c74a46a0c3e7d0
Joseph Bleau <jbleau@gmail.com>
2013-11-05 10:20:28 -0500

    wired newly created services to default controller

--- src/main/java/com/systemsinmotion/orgchart/web/controller/DefaultController.java
+++ src/main/java/com/systemsinmotion/orgchart/web/controller/DefaultController.java
@@ -1,23 +1,20 @@
 package com.systemsinmotion.orgchart.web.controller;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.systemsinmotion.orgchart.entity.Department;
 import com.systemsinmotion.orgchart.service.DepartmentService;
+import com.systemsinmotion.orgchart.service.EmployeeService;
+import com.systemsinmotion.orgchart.service.JobTitleService;
 import com.systemsinmotion.orgchart.web.View;
 
 @Controller
@@ -27,16 +24,15 @@ public class DefaultController {
 	private static final Logger log = LoggerFactory
 			.getLogger(DefaultController.class);
 
-//	@Autowired
-//	EmployeeService employeeService;
+	@Autowired
+	EmployeeService employeeService;
 
 	@Autowired
 	DepartmentService departmentService;
 
-//	@Autowired
-//	JobTitleService jobTitleService;
+	@Autowired
+	JobTitleService jobTitleService;
 	
-
 	@RequestMapping(value = "home", method = RequestMethod.GET)
 	public String doGet() {
 		return View.HOME;
