505c9104c89ac861e4c231994df041f92960b708
Joseph Bleau <jbleau@gmail.com>
2013-11-05 10:05:44 -0500

    implemented basic job title dao

--- src/main/java/com/systemsinmotion/orgchart/dao/JobTitleDao.java
+++ src/main/java/com/systemsinmotion/orgchart/dao/JobTitleDao.java
@@ -2,38 +2,74 @@ package com.systemsinmotion.orgchart.dao;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+
 import com.systemsinmotion.orgchart.entity.JobTitle;
 
+@Repository("jobTitleDao")
 public class JobTitleDao implements IJobTitleDao {
 
+	private static final Logger LOG = LoggerFactory.getLogger(JobTitleDao.class);
+	
+	@Autowired
+	HibernateTemplate hibernateTemplate;
+	
+	/* (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.IJobTitleDao#delete(com.systemsinmotion.orgchart.entity.JobTitle)
+	 */
 	@Override
 	public void delete(JobTitle jobTitle) {
-		// TODO Auto-generated method stub
-
+		LOG.debug("Deleting Job Title " + jobTitle.getName());;
+		this.hibernateTemplate.delete(jobTitle);;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.IJobTitleDao#findAll()
+	 */
 	@Override
 	public List<JobTitle> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		LOG.debug("Listing all Job Titles");
+		
+		@SuppressWarnings("unchecked")
+		List<JobTitle> jobTitles = this.hibernateTemplate.find("from " + JobTitle.class.getName() + " order by name");
+		
+		return jobTitles;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.IJobTitleDao#findById(java.lang.Integer)
+	 */
 	@Override
 	public JobTitle findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		LOG.debug("Finding a Job Title with id " + id);
+		JobTitle jobTitle = null;
+		
+		if( null != id ) {
+			jobTitle = this.hibernateTemplate.get(JobTitle.class, id);
+		}
+		
+		return jobTitle;
 	}
 
+	/* (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.IJobTitleDao#save(com.systemsinmotion.orgchart.entity.JobTitle)
+	 */
 	@Override
 	public Integer save(JobTitle jobTitle) {
-		// TODO Auto-generated method stub
-		return null;
+		LOG.debug("Saving a new Job Title by name " + jobTitle.getName());
+		return (Integer) this.hibernateTemplate.save(jobTitle);
 	}
 
+	/* (non-Javadoc)
+	 * @see com.systemsinmotion.orgchart.dao.IJobTitleDao#update(com.systemsinmotion.orgchart.entity.JobTitle)
+	 */
 	@Override
 	public void update(JobTitle jobTitle) {
-		// TODO Auto-generated method stub
-
+		LOG.debug("Updating Job Title by name " + jobTitle.getName());
+		this.hibernateTemplate.update(jobTitle);
 	}
-
 }
