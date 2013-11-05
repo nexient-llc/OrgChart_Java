1a816af49daf9ec185c286c633f80be2e7fe9e4d
Joseph Bleau <jbleau@gmail.com>
2013-11-05 10:16:43 -0500

    implemented basic job title service based on department service

--- src/main/java/com/systemsinmotion/orgchart/service/JobTitleService.java
+++ src/main/java/com/systemsinmotion/orgchart/service/JobTitleService.java
@@ -1,5 +1,37 @@
 package com.systemsinmotion.orgchart.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.systemsinmotion.orgchart.dao.IJobTitleDao;
+import com.systemsinmotion.orgchart.entity.JobTitle;
+
+@Service("jobTitleService")
 public class JobTitleService {
+
+	@Autowired
+	IJobTitleDao jobTitleDao;
+
+	public JobTitle findJobTitletByID(Integer jobTitleId) {
+		return this.jobTitleDao.findById(jobTitleId);
+	}
+
+	public void setjobTitleDAO(IJobTitleDao jobTitleDao) {
+		this.jobTitleDao = jobTitleDao;
+	}
+
+	public List<JobTitle> findAlljobTitles() {
+		return this.jobTitleDao.findAll();
+	}
+
+	public Integer storeJobTitle(JobTitle department) {
+		return this.jobTitleDao.save(department);
+	}
+
+	public void removejobTitle(JobTitle department) {
+		this.jobTitleDao.delete(department);
+	}
 
 }
