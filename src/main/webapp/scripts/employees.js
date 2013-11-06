a2dd4bd714a05417da275c99f5173fac9c9dc458
Joseph Bleau <jbleau@gmail.com>
2013-11-06 16:00:37 -0500

    added validation to manager field, person cannot manage themselves, finished update script

--- src/main/webapp/scripts/employees.js
+++ src/main/webapp/scripts/employees.js
@@ -7,7 +7,11 @@
  * the <select> which has the value we provided
  * with val. It also updates the value appropriately.
  */
+
 function selectByValue(obj, val) {
+	if(val == undefined)
+		return;
+	
 	obj.val(val);
 	obj.each(function(){
 	    if ($(this).text() == val) {
@@ -25,12 +29,18 @@ $(document).ready(function() {
 		 * based on the passed in id.
 		 */
 		$.ajax({
-			url: "emps/" + $(this).data("id") + "/json",
+			url: "employees/" + $(this).data("id") + "/json",
 			type: "GET",
 			dataType: "json"
 		
 		}).done(function(employee){
+			/* Clear state */
+			$("#managerError").html("");
+			errors = [];
+			$("#editSubmit").attr("disabled", false);
+			 
 			/* Pre-fill edit forms. */
+			$("#id").val(employee.id);
 			$("#firstName").val(employee.firstName);
 			$("#lastName").val(employee.lastName);
 			$("#lastName").val(employee.lastName);
@@ -38,11 +48,16 @@ $(document).ready(function() {
 			$("#email").val(employee.email);
 			$("#isManager").prop('checked', employee.isManager	);
 			
+			if(employee.manager != undefined && employee.manager.id != undefined)
+				$("#manager").val(employee.manager.id);
+			else
+				$("#manager").val("");
+			
 			/* Select appropriate Job Title*/
-			selectByValue($('#jobTitles'), employee.jobTitle.id);
+			selectByValue($('#jobTitle'), employee.jobTitle.id);
 			
 			/* Select appropriate Department */
-			selectByValue($('#departments'), employee.department.id);
+			selectByValue($('#department'), employee.department.id);
 			
 			/* Check Is Manager box if they're a manager. Make sure
 			 * it's unchecked if they're not. */
@@ -51,17 +66,30 @@ $(document).ready(function() {
 			} else {
 				$('#isManager').prop('checked',false);
 			}
+			
+			/* Define onChange handler for managed by field to validate
+			 * against them managing themselves.
+			 */
+			
+			$("#manager").change(function(){
+				var ourId = $("#id").val();
+				if(ourId == $(this).val()) {
+					$(this).css({'background-color' : 'light red'});
+					$("#managerError").html("You cannot manage yourself<br>");
+					$("#editSubmit").attr("disabled", true);
+				} else {
+					$(this).css({'background-color' : 'white'});
+					$("#managerError").html("");
+					$("#editSubmit").attr("disabled",false);
+				}
+				
+			});
 		});
 		
 		$("#empEditDialog").dialog({
 			modal: true,
 			width: 400
 		});
-		
-		
-		/* Clear manager field on focus. */
-		$('#manager').on('click focusin', function() {
-		    this.value = '';
-		});
+	
 	});
 });
