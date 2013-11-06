ffeb400609fcbcb7dc836e1e46f1853dccd50dea
Joseph Bleau <jbleau@gmail.com>
2013-11-06 12:48:58 -0500

    added other entries

--- src/main/java/com/systemsinmotion/orgchart/web/ModelKey.java
+++ src/main/java/com/systemsinmotion/orgchart/web/ModelKey.java
@@ -3,4 +3,8 @@ package com.systemsinmotion.orgchart.web;
 public class ModelKey {
 	public static final String DEPARTMENT = "dept";
 	public static final String DEPARTMENTS = "depts";
+	public static final String EMPLOYEE = "emp";
+	public static final String EMPLOYEES = "emps";
+	public static final String JOB_TITLE = "job";
+	public static final String JOB_TITLES = "jobs";
 }
