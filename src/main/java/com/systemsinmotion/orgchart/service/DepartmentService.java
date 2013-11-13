30d82d66181eb7bef62a68a53df1962298870ebe
skronek <kskronek@systemsinmotion.com>
2013-11-13 12:19:28 -0500

    Name of reference object changed

--- src/main/java/com/systemsinmotion/orgchart/service/DepartmentService.java
+++ src/main/java/com/systemsinmotion/orgchart/service/DepartmentService.java
@@ -5,21 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.systemsinmotion.orgchart.dao.IDepartmentDao;
+import com.systemsinmotion.orgchart.dao.DepartmentDao;
 import com.systemsinmotion.orgchart.entity.Department;
 
 @Service("departmentService")
 public class DepartmentService {
 
 	@Autowired
-	IDepartmentDao departmentDAO;
+	DepartmentDao departmentDAO;
 
 	public Department findDepartmentByID(Integer departmentId) {
 
 		return this.departmentDAO.findById(departmentId);
 	}
 
-	public void setDepartmentDAO(IDepartmentDao deparmentDAO) {
+	public void setDepartmentDAO(DepartmentDao deparmentDAO) {
 		this.departmentDAO = deparmentDAO;
 	}
 
