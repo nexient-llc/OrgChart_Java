8824c883334b3cd7b6acf85d78ded683a780c948
skronek <kskronek@systemsinmotion.com>
2014-01-09 14:06:43 -0500

    Unused test configuration

---
+++ src/test/java/com/bestbuy/supportspace/videolibrary/config/TestConfig.java
@@ -0,0 +1,10 @@
+package com.bestbuy.supportspace.videolibrary.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"com.systemsinmotion.orgchart"})
+public class TestConfig {
+
+}
