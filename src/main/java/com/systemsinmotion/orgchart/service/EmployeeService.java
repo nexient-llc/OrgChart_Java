fc2500e8b64da53b55beb6d19ec58d84ece1cac1
mshih <mshih@systemsinmotion.com>
2014-01-20 17:40:44 -0500

    Last commit did not include EmployeeService and ServiceTest, recommiting

---
+++ src/main/java/com/systemsinmotion/orgchart/service/EmployeeService.java
@@ -0,0 +1,52 @@
+package com.systemsinmotion.orgchart.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.systemsinmotion.orgchart.data.EmployeeRepository;
+import com.systemsinmotion.orgchart.entity.Employee;
+
+@Service("employeeService")
+public class EmployeeService {
+	
+	@Autowired
+	EmployeeRepository employeeRepo;
+	
+	public List<Employee> findAllEmployees() {
+		return this.employeeRepo.findAll();
+	}
+
+	public Employee findEmployeeById(Integer employeeId) {
+		return this.employeeRepo.findOne(employeeId);
+	}
+	
+	public Employee findEmployeeByFirstName(String firstName) {
+		return this.employeeRepo.findEmployeeByFirstName(firstName);
+	}
+	
+	public Employee findEmployeeByLastName(String lastName) {
+		return this.employeeRepo.findEmployeeByLastName(lastName);
+	}
+	
+	public Employee findEmployeeByEmail(String email) {
+		return this.employeeRepo.findEmployeeByEmail(email);
+	}
+	
+	public Employee findEmployeeBySkypeName(String skypeName) {
+		return this.employeeRepo.findEmployeeBySkypeName(skypeName);
+	}
+	
+	public void setRepository(EmployeeRepository employeeRepo) {
+		this.employeeRepo = employeeRepo;
+	}
+	
+	public void removeEmployee(Employee employee) {
+		this.employeeRepo.delete(employee);
+	}
+	
+	public Employee storeEmployee(Employee employee) {
+		return this.employeeRepo.save(employee);
+	}
+}
---
+++ src/test/java/com/systemsinmotion/orgchart/service/EmployeeServiceTest.java
@@ -0,0 +1,101 @@
+package com.systemsinmotion.orgchart.service;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.systemsinmotion.orgchart.Entities;
+import com.systemsinmotion.orgchart.data.EmployeeRepository;
+import com.systemsinmotion.orgchart.entity.Employee;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/test-context.xml")
+@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+@Transactional
+@WebAppConfiguration("/src/main/webapp")
+public class EmployeeServiceTest {
+
+	@Autowired
+	EmployeeService employeeService;
+	
+	EmployeeRepository mockEmployeeRepo = mock(EmployeeRepository.class);
+	Employee mockEmployee = mock(Employee.class);
+
+	private ArrayList<Employee> listOfFoundEmps = new ArrayList<Employee>();
+	
+	@Before
+	public void before() throws Exception {
+		when(this.mockEmployee.getId()).thenReturn(Entities.EMPLOYEE_ID);
+		this.listOfFoundEmps.add(this.mockEmployee);
+		when(this.mockEmployeeRepo.findAll()).thenReturn(this.listOfFoundEmps);
+		when(this.mockEmployeeRepo.findEmployeeByFirstName(Entities.FIRST_NAME)).thenReturn(this.mockEmployee);
+		when(this.mockEmployeeRepo.findEmployeeByLastName(Entities.LAST_NAME)).thenReturn(this.mockEmployee);
+		when(this.mockEmployeeRepo.findEmployeeByEmail(Entities.EMAIL)).thenReturn(this.mockEmployee);
+		when(this.mockEmployeeRepo.findEmployeeBySkypeName(Entities.SKYPE_NAME)).thenReturn(this.mockEmployee);
+		when(this.mockEmployeeRepo.findOne(Entities.EMPLOYEE_ID)).thenReturn(this.mockEmployee);
+		when(this.mockEmployeeRepo.save(this.mockEmployee)).thenReturn(this.mockEmployee);
+		this.employeeService.setRepository(this.mockEmployeeRepo);
+	}
+	
+	@Test
+	public void findAllEmployees() {
+		List<Employee> emps = this.employeeService.findAllEmployees();
+		assertNotNull(emps);
+		assertEquals(1, emps.size());
+	}
+	
+	@Test
+	public void findEmployeeById() {
+		Employee emps = this.employeeService.findEmployeeById(Entities.EMPLOYEE_ID);
+		assertNotNull(emps);
+		assertEquals(Entities.EMPLOYEE_ID, emps.getId());
+	}
+	
+	@Test
+	public void findEmployeeByFirstName() {
+		Employee emps = this.employeeService.findEmployeeByFirstName(Entities.FIRST_NAME);
+		assertNotNull(emps);
+		assertEquals(Entities.EMPLOYEE_ID, emps.getId());
+	}
+	
+	@Test
+	public void findEmployeeByLastName() {
+		Employee emps = this.employeeService.findEmployeeByLastName(Entities.LAST_NAME);
+		assertNotNull(emps);
+		assertEquals(Entities.EMPLOYEE_ID, emps.getId());
+	}
+	
+	@Test
+	public void findEmployeeByEmail() {
+		Employee emps = this.employeeService.findEmployeeByEmail(Entities.EMAIL);
+		assertNotNull(emps);
+		assertEquals(Entities.EMPLOYEE_ID, emps.getId());
+	}
+	
+	@Test
+	public void findEmployeeBySkypeName() {
+		Employee emps = this.employeeService.findEmployeeBySkypeName(Entities.SKYPE_NAME);
+		assertNotNull(emps);
+		assertEquals(Entities.EMPLOYEE_ID, emps.getId());
+	}
+	
+	@Test
+	public void storeEmployee() {
+		Integer empId = this.employeeService.storeEmployee(this.mockEmployee).getId();
+		assertNotNull(empId);
+		assertEquals(Entities.EMPLOYEE_ID, empId);
+	}
+}
