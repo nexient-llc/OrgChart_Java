b1bc885eab3d37fd61ca062856d7ea7acfc0b74f
cmora <cmora@systemsinmotion.com>
2014-07-15 10:03:10 -0400

    Base repo handles findAllActive

---
+++ src/main/java/com/systemsinmotion/orgchart/data/BaseRepository.java
@@ -0,0 +1,15 @@
+package com.systemsinmotion.orgchart.data;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+import com.systemsinmotion.orgchart.entity.BaseEntity;
+
+@NoRepositoryBean
+public interface BaseRepository<T extends BaseEntity, ID extends Serializable> extends JpaRepository<T,ID>{
+	
+	 List<T> findByIsActiveIsTrue();
+
+}
