e459bfb733662e7f628f2875a6e1406c5f36cca7
Jon Green <jgreen@systemsinmotion.com>
2014-07-23 16:01:57 -0400

    Most of EmployeeRepository tests are running

--- src/main/java/com/systemsinmotion/orgchart/data/EmployeeRepository.java
+++ src/main/java/com/systemsinmotion/orgchart/data/EmployeeRepository.java
@@ -7,15 +7,15 @@ import com.systemsinmotion.orgchart.entity.Employee;
 
 public interface EmployeeRepository extends BaseRepository<Employee,Integer>{
 
-//	List<Employee> findByDepartment(Department dept);
+	List<Employee> findByDepartment(Department dept);
 
-//	Employee findByEmail(String email);
+	Employee findByEmail(String email);
 
 	Employee findById(Integer id);
 
-//	List<Employee> findByManager(Employee manager);
+	List<Employee> findByManager(Employee manager);
 
-//	List<Employee> findByFirstNameAndLastNameAndDepartmentIdAndJobTitleId(
-//			String firstName, String lastName, Integer id, Integer id2);
+	List<Employee> findByFirstNameAndLastNameAndDepartmentIdAndJobTitleId(
+			String firstName, String lastName, Integer id, Integer id2);
 
 }
