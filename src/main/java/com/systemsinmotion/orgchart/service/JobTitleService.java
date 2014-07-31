ba1701bca07cc714b4595a78864f59218cf9aa84
Jon Green <jgreen@systemsinmotion.com>
2014-07-31 12:15:05 -0400

    Fixed problem with eod 7/30 commit thanks to git log -p

--- src/main/java/com/systemsinmotion/orgchart/service/JobTitleService.java
+++ src/main/java/com/systemsinmotion/orgchart/service/JobTitleService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.systemsinmotion.orgchart.data.JobTitleRepository;
-import com.systemsinmotion.orgchart.entity.Employee;
 import com.systemsinmotion.orgchart.entity.JobTitle;
 
 @Service("jobTitleService")
@@ -21,10 +20,6 @@ public class JobTitleService {
 
 	public JobTitle storeJobTitle(JobTitle jobTitle) {
 		return this.repository.save(jobTitle);
-	}
-
-	public void storeEmployee(JobTitle job) {
-		this.repository.save(job);
 	}
 
 	public List<JobTitle> findAllActiveJobTitles() {
